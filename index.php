55e64b7a4306bf6c59746e781415769fca9acb99
YesseniaUC <yalenaga@gmail.com>
2024-04-03 21:02:47 -0600

    cambios index

--- index.php
+++ index.php
@@ -4,40 +4,87 @@
 <main>
 
     <section class="row justify-content-evenly">
-        <h2 class="col-sm-12">cartelera</h2>
+        <h2 class="col-sm-12">Casas en Venta</h2>
 
         <div class="card col-md-6 col-lg-4" style="width: 18rem;">
             <img src="img/CASA 2.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Guardianes de la Galaxia</h5>
-                <p class="card-text">Breve Descripción.</p>
-                <a href="#" class="btn btn-rojo">Ver Horarios</a>
+                <h5 class="card-title">Casa en Escazú</h5>
+                <p class="card-text">200 m2</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
             </div>
         </div>
         <div class="card col-md-6 col-lg-4" style="width: 18rem;">
-            <img src="img/estreno6.png" class="card-img-top" alt="...">
+            <img src="img/CASA 3.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Guardianes de la Galaxia</h5>
-                <p class="card-text">Breve Descripción.</p>
-                <a href="#" class="btn btn-rojo">Ver Horarios</a>
+                <h5 class="card-title">Casa en Curridabat</h5>
+                <p class="card-text">350 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
             </div>
         </div>
         <div class="card col-md-6 col-lg-4" style="width: 18rem;">
-            <img src="img/estreno2.png" class="card-img-top" alt="...">
+            <img src="img/Casa 4.avif" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Guardianes de la Galaxia</h5>
-                <p class="card-text">Breve Descripción.</p>
-                <a href="#" class="btn btn-rojo">Ver Horarios</a>
+                <h5 class="card-title">Casa en Heredia</h5>
+                <p class="card-text">1000 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
             </div>
         </div>
         <div class="card col-md-6 col-lg-4" style="width: 18rem;">
-            <img src="img/estreno3.png" class="card-img-top" alt="...">
+            <img src="img/Casa 6.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Guardianes de la Galaxia</h5>
+                <h5 class="card-title">Casa en Sabana Norte</h5>
+                <p class="card-text">500 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
+            </div>
+        </div>
+        <div class="card col-md-6 col-lg-4" style="width: 18rem;">
+            <img src="img/Casa 5.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Casa en Santa Ana</h5>
+                <p class="card-text">800 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
+            </div>
+        </div>
+        <div class="card col-md-6 col-lg-4" style="width: 18rem;">
+            <img src="img/Casa 7.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Casa en Guanacaste</h5>
+                <p class="card-text">2000 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
+            </div>
+        </div>
+        <div class="card col-md-6 col-lg-4" style="width: 18rem;">
+            <img src="img/Casa 8 .jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Casa en Cartago</h5>
+                <p class="card-text">2500 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
+            </div>
+        </div>
+        <div class="card col-md-6 col-lg-4" style="width: 18rem;">
+            <img src="img/Casa 9.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Casa en San Carlos</h5>
+                <p class="card-text">1500 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
+            </div>
+        </div>
+        <div class="card col-md-6 col-lg-4" style="width: 18rem;">
+            <img src="img/Casa 11.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Casa en Jacó</h5>
+                <p class="card-text">2500 m2.</p>
+                <a href="#" class="btn btn-rojo">Ver Detalles</a>
+            </div>
+        </div>
+        <div class="card col-md-6 col-lg-4" style="width: 18rem;">
+            <img src="img/Casa 10.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Casa en Monte la Cruz</h5>
                 <p class="card-text">Breve Descripción.</p>
                 <a href="#" class="btn btn-rojo">Ver Horarios</a>
             </div>
-        </div>
 
 
     </section>
