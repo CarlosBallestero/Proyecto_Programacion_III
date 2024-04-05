66238c8f5ac771f487a2e6c5adc7e3cb5f956e23
YesseniaUC <yalenaga@gmail.com>
2024-04-04 20:04:13 -0600

    cambios header.php

--- shared/header.php
+++ shared/header.php
@@ -7,8 +7,8 @@ $url = basename($_SERVER["PHP_SELF"], ".php");
 
 <head>
     <meta charset="UTF-8">
-    <meta name="author" content="Rolando Sequeira">
-    <meta name="description" content="Página principal de .....">
+    <meta name="author" content="Tare Blanco Gutiérrez, Carlos Ballestero, Yessenia Umaña Cervantes">
+    <meta name="description" content="Página principal de Bienes Raices">
     <meta name="keywords" content="restaurante, comida china, china">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Primer Sitio</title>
@@ -22,7 +22,7 @@ $url = basename($_SERVER["PHP_SELF"], ".php");
         <nav class="navbar navbar-expand-lg ">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">
-                    <img src="img/logo.png" alt="Logo de la empresa" width="50">
+                    <img src="img/Logo proyecto.png" alt="Logo de la empresa" width="80">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -30,13 +30,16 @@ $url = basename($_SERVER["PHP_SELF"], ".php");
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="acerca.php">Acerca</a>
+                            <a class="nav-link active" aria-current="page" href="acerca.php">Aun te interesa</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="servicios.php">Servicios</a>
+                            <a class="nav-link" href="servicios.php">¿Por qué elegirnos?</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contactenos.php">Contáctenos</a>
+                            <a class="nav-link" href="contactenos.php">Nuestras propiedades</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contactenos.php">Testimonios</a>
                         </li>
                         <!-- <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
