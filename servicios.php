d747021fbe08893ff9a80cbe7553ac45b6fb5297
TareIBG <tblanco119160584@gmail.com>
2024-04-05 21:39:28 -0600

    cambios pq

--- servicios.php
+++ servicios.php
@@ -1,5 +1,13 @@
-<?php include "shared/header.php" ?>
+<?php include "shared/header.php"; ?>
 <main>
-    <h1>¿Por qué elegirnos?</h1>
+    <<div class="card col-md-6 col-lg-4" style="width: 107rem;">
+            <div class="card-body">
+                <h5 class="card-title">¿Por qué elegirnos?</h5>
+                <p class="card-title">Somos una empresa comprometida con la calidad y la satisfacción del cliente. Nuestro equipo altamente capacitado se esfuerza por ofrecer productos y servicios de excelencia que superen las expectativas.</p>
+                <p class="card-title">Además, contamos con años de experiencia en el mercado, lo que nos ha permitido desarrollar un profundo conocimiento de las necesidades de nuestros clientes y adaptarnos de manera eficiente a sus requerimientos.</p>
+                <p  class="card-title">No solo nos enfocamos en brindar soluciones efectivas, sino que también nos preocupamos por mantener una comunicación abierta y transparente con nuestros clientes, garantizando así una relación de confianza mutua.</p>
+                <p  class="card-title">En resumen, al elegirnos, estás optando por una empresa que se preocupa genuinamente por tu satisfacción y que tiene la experiencia y el compromiso necesarios para cumplir con tus expectativas.</p>
+            </div>
+        </div>
 </main>
-<?php include "shared/footer.php" ?>
+<?php include "shared/footer.php";?>
